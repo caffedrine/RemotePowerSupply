4885d7ce1b9125bca486e30ff527f331b54141ec
caffedrine <bacchusstyle@yahoo.com>
2021-12-07 17:23:00 +0200

    - enable debugging messages when app launched from console - migrated to cmakelists

---
+++ Qt/RemotePowerSupply/src/main.cpp
@@ -0,0 +1,24 @@
+#include "mainwindow.h"
+#include <QApplication>
+#include <Windows.h>
+
+int main(int argc, char *argv[])
+{
+    // Show debug info when app is executed from console
+    #ifdef _WIN32
+        /// https://stackoverflow.com/questions/3360548/console-output-in-a-qt-gui-app
+        if (AttachConsole(ATTACH_PARENT_PROCESS))
+        {
+            freopen("CONOUT$", "w", stdout);
+            freopen("CONOUT$", "w", stderr);
+        }
+    #endif
+
+    qputenv("QT_MESSAGE_PATTERN", QByteArray("[%{time yyyy-MM-dd h:mm:ss.zzz}] %{file}:%{line} - %{message}"));
+
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+
+    return a.exec();
+}
